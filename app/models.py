7a318caeb7920ae5eebb885d3a47f050d844ed8e
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-27 20:29:06 +0300

    it will define article class and then create an __init__ method and pass in six parameters needed inside our source objects.

--- app/models.py
+++ app/models.py
@@ -7,3 +7,16 @@ class Source:
         self.name = name
         self.description = description
         self.url = url
+
+class Article:
+    '''
+    Article class to define Article Objects
+    '''
+
+    def __init__(self,author,title,description,url,poster,publishedAt):
+        self.author =author
+        self.title = title
+        self.description = description
+        self.link=url
+        self.poster = poster
+        self.publishedAt= publishedAt
