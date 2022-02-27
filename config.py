409ac180461c2b7b6a746d8f472434bfae5acd22
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-27 21:03:41 +0300

    it will import the os module that will allow interactivity with the os dependent functionality

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?{}&apiKey={}'
-    NEWS_API_KEY = 'NEWS_API_KEY'
+    ARTICLE_API_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
