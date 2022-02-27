fcab38a7e9c5a436a71b37039fb7131dd3434ccc
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-27 14:30:24 +0300

    it will  use the get function to get more news categories and then we pass them into our template.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from email import message
 from turtle import title
-from flask import render_template
+from flask import render_template,url_for
 from . import main
+from ..requests import get_source, get_news
+from ..models import Source, Article
 
 # Views
 @main.route('/')
@@ -10,5 +12,20 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Home- Welcome to The Best News Preview Articles Website'
-    return render_template('..index.html', title = title)
+    newsource=get_source('sources')
+
+    title = 'Home- Welcome to The Best News Articles Preview Website'
+    return render_template('..index.html', title = title, sources=newsource)
+
+
+
+@main.route('/articles/<source>') 
+def articles(source):
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    articles=get_news(source)
+    title='Articles - Welcome to The Best News Articles Preview Website' 
+
+    return render_template('articles.html',title=title,articles=articles,source=source)
